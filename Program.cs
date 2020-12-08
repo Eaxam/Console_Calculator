c640658348f34a5d5eef65dd32884497677faef7
Exam <66176604+Eaxam@users.noreply.github.com>
2020-12-08 14:22:01 +0200

    add main code

--- Program.cs
+++ Program.cs
@@ -6,7 +6,65 @@ namespace Console_Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Double number1;
+            Double number2;
+            string operation;
+
+            Console.WriteLine("Wtite Number 1");
+
+            number1 = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine(" Write Operation + - * / ");
+
+            operation = Console.ReadLine();
+            
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine("Write Number 2");
+
+                    number2 = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine(number1 + number2);
+                    break;
+
+                   
+                case "-":
+                    Console.WriteLine("Write Number 2");
+                    
+                    number2 = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine(number1 - number2);
+                    break;
+
+                case "*":
+                    Console.WriteLine("Write Number 2");
+                    
+                    number2 = double.Parse(Console.ReadLine());
+                    Console.WriteLine(number1 * number2);
+                    break;
+
+                case "/" :
+                    
+                    Console.WriteLine("Write Number 2");
+                    
+                    number2 = double.Parse(Console.ReadLine());
+                    if (number2 == 0)
+                    {
+                        Console.WriteLine("ERROR");
+                    }
+                    else
+                    {
+                        Console.WriteLine(number1 / number2);
+                    }
+                    break;
+                default:
+                    Console.WriteLine("ERRoR");
+                    break;
+            }   
+
+            Console.ReadLine();
+
         }
-    }
+    }   
 }
